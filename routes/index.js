89e92bf55fbf1b2d3f8a42a46be02e49cefc3e8f
LizTheDeveloper <liz.kelner@gmail.com>
2015-04-27 10:23:47 -0700

    Got data pulling in from google spreadsheets

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var GoogleSpreadsheet = require("google-spreadsheet");
+var _ = require('underscore');
+ 
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	// spreadsheet key is the long id in the sheets URL 
+	var my_sheet = new GoogleSpreadsheet('1ggKZUjEVoiuiXUXxThM5xMRzEW-V7XtcYckYEBOU8lA');
+	 
+	// Without auth -- read only 
+	// IMPORTANT: See note below on how to make a sheet public-readable! 
+	// # is worksheet id - IDs start at 1 
+	my_sheet.getRows( 1, function(err, row_data){
+		// loop through all the rows
+		// turn the submit date into a JS date
+		// compare with today
+		// remove from list if they are not valid
+	    res.render('index', { sheet_rows: row_data });
+	});
 });
 
 module.exports = router;
