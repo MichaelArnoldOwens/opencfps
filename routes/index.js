3462cefe0ff8b5b0425f154558aceb7fb5a7d230
LizTheDeveloper <liz.kelner@gmail.com>
2015-04-27 10:36:16 -0700

    added filtering, and a link to submit

--- routes/index.js
+++ routes/index.js
@@ -16,12 +16,20 @@ router.get('/', function(req, res, next) {
 	// Without auth -- read only 
 	// IMPORTANT: See note below on how to make a sheet public-readable! 
 	// # is worksheet id - IDs start at 1 
-	my_sheet.getRows( 1, function(err, row_data){
-		// loop through all the rows
-		// turn the submit date into a JS date
-		// compare with today
-		// remove from list if they are not valid
-	    res.render('index', { sheet_rows: row_data });
+	my_sheet.getRows( 1, function(err, all_dates){
+		// get upcoming dates only
+		var upcomingProposalDates = _.reject(all_dates, function(row) {
+			var submitDate = new Date(row['whensthesubmissiondeadline'])
+			return submitDate < new Date();
+		});
+		// get passed dates only
+		var passedProposalDates = _.reject(all_dates, function(row) {
+			var submitDate = new Date(row['whensthesubmissiondeadline'])
+			return submitDate > new Date();
+		});
+
+		//render and send
+	    res.render('index', { upcoming_dates: upcomingProposalDates, passed_dates: passedProposalDates });
 	});
 });
 
